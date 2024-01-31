71926e5790e8cf8650f0ace74f0b8878629b43aa
minmeanmin <wed1604@naver.com>
2024-01-31 09:44:22 +0900

    로그인 오류 해결

--- src/main/java/shop/mtcoding/blog/user/UserRepository.java
+++ src/main/java/shop/mtcoding/blog/user/UserRepository.java
@@ -41,7 +41,12 @@ public class UserRepository {
         query.setParameter(1, requestDTO.getUsername());
         query.setParameter(2, requestDTO.getPassword());
 
-        User user = (User) query.getSingleResult();
-        return user;
+        try{
+            User user = (User) query.getSingleResult();
+
+            return user;
+        }catch (Exception e){
+            return null;
+        }
     }
 }
